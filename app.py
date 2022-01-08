30bf008c8c5ee9aedb3cc87cff7d432b6c5a96bc
Yuta123456 <yuuta09090530@icloud.com>
2022-01-08 17:30:59 +0900

    認証部分を作成

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
 )
-
+import requests
 import datetime
 from messages.question import question
-from util.firebase import post_data, show_data
+from util.firebase import is_exists_x_site_code, post_data, save_LINE_user_id_and_x_site_code, show_data
 from util.message import get_fruits, get_no_reply_message
 from constants.LINE_BOT import LINE_BOT_CHANNEL_SECRET, LINE_BOT_CHANNEL_TOKEN
 app = Flask(__name__)
@@ -78,6 +78,42 @@ def handle_message(event):
         event.reply_token,
         message)
 
-
+@app.route("/auth", methods=['POST'])
+def auth():
+    # https://example.com/auth?code=abcd1234&state=0987poi&friendship_status_changed=true
+    # 認可コード取得
+    authorization_code = request.args.get("code")
+    # クロスサイトリクエストフォージェリ防止用の固有な英数字の文字列
+    prevent_X_site_request_forgery_code = request.args.get("state")
+    if (not is_exists_x_site_code(prevent_X_site_request_forgery_code)):
+        # 失敗 コードは考える
+        return 400
+    # アクセストークン取得
+    access_token = getLINEAccessToken(authorization_code)
+    userId = getLINEProfile(access_token)
+    save_LINE_user_id_and_x_site_code(userId, prevent_X_site_request_forgery_code)
+    return 200
+def getLINEAccessToken(authorization_code):
+    url = "https://api.line.me/oauth2/v2.1/token"
+    header = {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+    }
+    payload = {
+        'grant_type': 'authorization_code',
+        'code': 'authorization_code',
+        'redirect_uri':"/auth" 
+    }
+    res = requests.post(url, headers=header, params=payload)
+    
+def getLINEProfile(access_token):
+    url = "https://api.line.me/v2/profile"
+    headers = {
+        'Authorization': f"Bearer{access_token}",
+    }
+    res = requests.get(url, headers=headers)
+    print(res.json())
+    # TODO: json化した後どんな感じになるかわからないのでいったん保留
+    # userId = res.json()
+    
 if __name__ == "__main__":
     app.run()

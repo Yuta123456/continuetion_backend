97bf8cc8bd5e34d6d9536854ee2e2692a83516cd
Yuta123456 <yuuta09090530@icloud.com>
2022-05-29 16:03:18 +0900

    helpを修正

--- util/message.py
+++ util/message.py
@@ -9,6 +9,7 @@ def get_no_reply_message():
     no_reply_message += "\nShow: 直近の継続データを表示します"
     no_reply_message += "\nRecord: 記録を取るためのメッセージを送ります"
     no_reply_message += "\nFruits: ランダムなフルーツを送ります"
+    no_reply_message += "\nset:継続内容: 継続内容をセットします"
     return no_reply_message
 
 def get_set_complete_message(contents):

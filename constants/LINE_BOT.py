60c4f6459fcb70ecfb2a612f773422f5d54d8c72
Yuta123456 <yuuta09090530@icloud.com>
2022-06-03 12:24:29 +0900

    ローカル開発のバグを修正

--- constants/LINE_BOT.py
+++ constants/LINE_BOT.py
@@ -2,7 +2,7 @@ import os
 line_key_path = './constants/seckey.txt'
 if os.path.exists(line_key_path):
     with open(line_key_path, mode='r') as f:
-        key = [s.sprit() for s in f.readlines()]
+        key = [s for s in f.readlines()]
         LINE_BOT_CHANNEL_SECRET = key[0]
         LINE_BOT_CHANNEL_TOKEN  = key[1]
 else:
